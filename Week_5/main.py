e8ae0b925af8d64028fdfb55f731188fbeaa3bbe
Wojtecki <benjamin.wojtecki@ens-rennes.fr>
2025-11-23 21:08:27 +0100

    add file

---
+++ Week_5/main.py
@@ -0,0 +1,32 @@
+import mmh3
+import math
+import random 
+
+sizeMax = 1000000
+nbHashes = 7
+
+bitsArr = [0] * sizeMax
+seeds =[]
+
+for _ in range(nbHashes):
+    seeds.append(random.randint(0, 1000))
+
+def addElement(elt):
+    for seed in seeds:
+        index = mmh3.hash(str(elt), seed, signed=False) % sizeMax
+        bitsArr[index] = 1
+
+def containsElement(elt):
+    for seed in seeds:
+        index = mmh3.hash(str(elt), seed, signed=False) % sizeMax
+        if bitsArr[index] == 0:
+            return False
+    return True
+
+addElement("Hello")
+addElement("GoodBye")
+
+print(containsElement("Hello"))
+print(containsElement("GoodBye"))
+print(containsElement("Hi"))
+print(containsElement("bonjour"))
